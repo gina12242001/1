09b76d294a15753899813c8042faea7e5fe4502f
Gina Lee <ginalee@Ginas-MacBook-Pro.local>
2020-08-04 13:52:16 +0900

    two functions

--- sample.py
+++ sample.py
@@ -1,1 +1,39 @@
 "Gina"
+# for i in range(1,30):
+# 	print i
+
+
+def function(x):
+	answer = 0
+	for i in range(0, len(str(x))):
+		answer = (x % 10) + answer   
+		x = x / 10 
+		print answer 
+
+	return answer
+	
+print function(1234)
+
+
+
+
+
+
+
+
+
+
+
+
+
+def multiply(x):
+	finalanswer = 1
+	for i in range(0,len(str(x))):
+		finalanswer = x % 10 * finalanswer
+		print finalanswer
+		x = x / 10 
+		print finalanswer
+	return finalanswer
+
+print multiply(1234)
+
